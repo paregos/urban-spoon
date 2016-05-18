d94cc83872e31c96305227132a25347de2ed195a
Paregos <ben.mitchell58@gmail.com>
2016-05-18 17:31:43 +1200

    Created light setting's for every room

--- content.js
+++ content.js
@@ -1,51 +1,89 @@
 $(document).ready(function() {
     $(".Bedroom1").click(function() {
-        var Id = $('.btn.btn-primary.active').attr('id');
+      var Id = $('.btn.btn-primary.active').attr('id');
 
+      if (Id == "bulbdiv"){
         $('#house').fadeOut(function(){
             $('#Bedroom1Lights').fadeIn();
         });
-        //your code here
+      }
+
     });
 });
 
 $(document).ready(function() {
     $(".Bedroom2").click(function() {
         var Id = $('.btn.btn-primary.active').attr('id');
-        alert(Id);
-        //your code here
+
+        if (Id == "bulbdiv"){
+          $('#house').fadeOut(function(){
+              $('#Bedroom2Lights').fadeIn();
+          });
+        }
+
     });
 });
 
 $(document).ready(function() {
     $(".Entrance").click(function() {
         var Id = $('.btn.btn-primary.active').attr('id');
-        alert(Id);
-        //your code here
+
+        if (Id == "bulbdiv"){
+          $('#house').fadeOut(function(){
+              $('#EntranceLights').fadeIn();
+          });
+        }
+
     });
 });
 
 $(document).ready(function() {
     $(".MasterBedroom").click(function() {
         var Id = $('.btn.btn-primary.active').attr('id');
-        alert(Id);
-        //your code here
+
+        if (Id == "bulbdiv"){
+          $('#house').fadeOut(function(){
+              $('#MasterBedroomLights').fadeIn();
+          });
+        }
+
     });
 });
 
 $(document).ready(function() {
     $(".Kitchen").click(function() {
-        var Id = $('.btn.btn-primary.active').attr('id');
-        alert(Id);
-        //your code here
+      var Id = $('.btn.btn-primary.active').attr('id');
+
+      if (Id == "bulbdiv"){
+        $('#house').fadeOut(function(){
+            $('#KitchenLights').fadeIn();
+        });
+      }
+
     });
 });
 
 $(document).ready(function() {
     $(".LivingRoom").click(function() {
         var Id = $('.btn.btn-primary.active').attr('id');
-        alert(Id);
-        //your code here
+
+        if (Id == "bulbdiv"){
+          $('#house').fadeOut(function(){
+              $('#LivingRoomLights').fadeIn();
+          });
+        }
+
+    });
+});
+
+//fade out of the current menu and back to the origional menu
+$(document).ready(function() {
+    $(".return").click(function() {
+
+        $('.settings').fadeOut(function(){
+            $('#house').fadeIn(1000);
+        });
+
     });
 });
 
