f63bab4734d7684af0a43fb5b7344b244c57010b
Paregos <ben.mitchell58@gmail.com>
2016-05-21 13:27:05 +1200

    created animations for the overlay buttons

---
+++ js/content.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+
+    onWindowResize();
+
+    //Opening Bedroom1
+    $(".Bedroom1").click(function() {
+        var Id = $('.btn.btn-primary.active').attr('id');
+
+        if (Id == "bulbdiv") {
+            $('#house').fadeOut(function() {
+                $('#Bedroom1Lights').fadeIn();
+            });
+        }
+        if (Id == "cameradiv") {
+            $('#house').fadeOut(function() {
+                $('#BedRoom1Camera').fadeIn();
+            });
+        }
+    });
+
+    //Opening Bedroom2
+    $(".Bedroom2").click(function() {
+        var Id = $('.btn.btn-primary.active').attr('id');
+
+        if (Id == "bulbdiv") {
+            $('#house').fadeOut(function() {
+                $('#Bedroom2Lights').fadeIn();
+            });
+        }
+    });
+
+    //Entrance
+    $(".Entrance").click(function() {
+        var Id = $('.btn.btn-primary.active').attr('id');
+
+        if (Id == "bulbdiv") {
+            $('#house').fadeOut(function() {
+                $('#EntranceLights').fadeIn();
+            });
+        }
+    });
+
+    //MasterBedroom
+    $(".MasterBedroom").click(function() {
+        var Id = $('.btn.btn-primary.active').attr('id');
+
+        if (Id == "bulbdiv") {
+            $('#house').fadeOut(function() {
+                $('#MasterBedroomLights').fadeIn();
+            });
+        }
+    });
+
+    //Kitchen
+    $(".Kitchen").click(function() {
+        var Id = $('.btn.btn-primary.active').attr('id');
+
+        if (Id == "bulbdiv") {
+            $('#house').fadeOut(function() {
+                $('#KitchenLights').fadeIn();
+            });
+        }
+    });
+
+    //LivingRoom
+    $(".LivingRoom").click(function() {
+        var Id = $('.btn.btn-primary.active').attr('id');
+
+        if (Id == "bulbdiv") {
+            $('#house').fadeOut(function() {
+                $('#LivingRoomLights').fadeIn();
+            });
+        }
+    });
+
+
+    //=========Changing the values of the light sliders
+    $('.slider').change(function() {
+        var Id = "." + $(this).attr('id');
+
+        if ($(this).val() == 0) {
+            $(Id).html("OFF");
+        } else {
+            $(Id).html($(this).val() + "%");
+        }
+
+    });
+
+
+    //========Return to floor plan from settings
+    $(".return").click(function() {
+        $('.settings').fadeOut(function() {
+            $('#house').fadeIn(1000);
+        });
+        onWindowResize();
+    });
+
+    //=========Icon Hover/Click Section============
+    $('.Icons').click(function() {
+        $('.Icons').addClass("greyed");
+        $(this).removeClass("greyed");
+    });
+
+
+});
